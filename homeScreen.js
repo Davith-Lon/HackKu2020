28b0974884ed201e2e7c561621bfb9e6ad81afaa
Davith Lon <davith.lonn@gmail.com>
2020-02-08 07:46:09 -0600

    Moved all styles to CSS file

--- homeScreen.js
+++ homeScreen.js
@@ -1,13 +1,5 @@
 var context, canvas, button1;
-let canvas = document.getElementById("homeScreen");
-let upperRec = canvas.getContext("2d");
 button1 = document.querySelector("startButton").getContext("2d");
-
-upperRec.fillStyle = "#202020";
-upperRec.fillRect(0,0,1582,750);
-
-context.fillStyle = "#000000";
-context.fillRect(0, 750, 1582, 50);
 
 function start(){
     window.location="index.html";
