f8d749cf087e4bf90418762f1a0b15902f056f57
Daniel Johnson <danielrjohnson00@gmail.com>
2020-02-07 21:43:50 -0600

    functioning pygame

--- Project.py
+++ Project.py
@@ -1,2 +1,7 @@
-def helloWorld():
-    print("Hello, World!")
+import pygame
+size = width,height = 400,400
+screen = pygame.display.set_mode(size)
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
