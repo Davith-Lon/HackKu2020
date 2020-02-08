437ce93b878fdcd96823575165f99ae9688011b0
Davith Lon <davith.lonn@gmail.com>
2020-02-08 08:23:25 -0600

    Added a black container for the homeScreen and added it to the CSS

--- homeScreen.js
+++ homeScreen.js
@@ -1,6 +1,20 @@
-var context, canvas, button1;
+var homeAnimation, button1;
 button1 = document.querySelector("startButton").getContext("2d");
 
 function start(){
     window.location="index.html";
 }
+
+var homeAnimation = document.getElementById("homeAnimate");   
+  var pos = 0;
+  var id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+      
+    } else {
+      pos++; 
+      elem.style.top = pos + "px"; 
+      elem.style.left = pos + "px"; 
+    }
+  }
