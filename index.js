3db7711847485d65d57b5731705b4f4413c12093
gholmesss <g.holmes429@gmail.com>
2020-02-08 23:55:54 -0600

    final changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var canvas, context, controller, rectangle, gameLoop, weather, weatherColor, Locations, key, rand, grSun, grRain , grSnow;
+var canvas, context, controller, rectangle, gameLoop, weather, weatherColor, Locations, key, rand, grSun, grRain , grSnow, globalSpeed=5;
 
 locations = [328846]
 key = '39KfKD60bv3lZ6CC6qCBMF5ZSfKo3ukU';
@@ -7,17 +7,17 @@ rand = Math.random(1);
 async function getWeather() {
     const response = await fetch('https://dataservice.accuweather.com/currentconditions/v1/328846?apikey=NJt0OiaGOOS4tXiwUXwGw2LUsOA0vQMe');
     const json = await response.json();
-    console.log(json);
+    //console.log(json);
     return json.WeatherText;
 }
-console.log(getWeather());
+//console.log(getWeather());
 weather = getWeather();
 
 if (weather != "Sunny"){
     if (weather != "Rain"){
         if (weather != "Snow"){
             rand = Math.floor(Math.random() * 3)
-            console.log(rand);
+            
             if (rand == 0){
                 weather = "Sunny";
             }
@@ -48,13 +48,16 @@ else if (weather == "Rain"){
     weatherColor = "#14C0F3";
 }
 
+var signal = true;
+var counter = -1;
+var score = 0;
 var numObstacles = 0;
 var obstacles = []
 var enemy;
-var jumpVel = 5;
-var jumpHeight = 75;
+var jumpVel = 6;
+var jumpHeight = 85;
 var currSd = 0;
-var glue = false;
+
 
 rectangle = {
     jumping: true,
@@ -75,6 +78,7 @@ class Obstacle {
         this.yPos = yPos;
         this.color = color;
         this.speed = speed;
+        this.glue = false;
         numObstacles += 1;
     };
     draw() {
@@ -113,16 +117,22 @@ function moveRect(){
     
     if (controller.up && rectangle.jumping == false){
         if (weather == "Snow"){
+            rectangle.yVel -= (jumpHeight-25);
+        }
+        else if (weather == "Rain") {
+            jumpVel = 6;
+            jumpHeight = 65;
             rectangle.yVel -= (jumpHeight-15);
+            
         }
         else {
-            rectangle.yVel -= (jumpHeight);
+            rectangle.yVel -= (jumpHeight-25);
         }
         rectangle.jumping = true;
     }
     if (controller.left){
         if (weather == "Snow"){
-            rectangle.xVel -= 1;
+            rectangle.xVel -= 1.75;
         }
         else {
             rectangle.xVel -= 2;
@@ -130,7 +140,7 @@ function moveRect(){
     }
     if (controller.right){
         if (weather == "Snow"){
-            rectangle.xVel += 1;
+            rectangle.xVel += 1.75;
         }
         else {
             rectangle.xVel += 2;
@@ -147,13 +157,15 @@ function moveRect(){
         rectangle.xVel *= 0.9;
         rectangle.yVel *= 0.9;
     }
-    if (glue == true&&!controller.left&&!controller.right&&rectangle.xVel >= -currSd && rectangle.xVel <= 0) {
+    for (i = 0; i < obstacles.length; i += 1) {
+        if (obstacles[i].glue == true&&!controller.left&&!controller.right&&rectangle.xVel >= -currSd && rectangle.xVel <= 0) {
 
         rectangle.xVel -= (currSd+rectangle.xVel); }
     
-    else if (glue == true&&!controller.left&&!controller.right&& currSd-rectangle.xVel>0 && rectangle.xVel >0) {
-            rectangle.xVel -= (currSd-rectangle.xVel);
-    }
+        else if (obstacles[i].glue == true&&!controller.left&&!controller.right&& currSd-rectangle.xVel>0 && rectangle.xVel >0) {
+                rectangle.xVel -= (currSd-rectangle.xVel);
+        }
+}
     if (rectangle.yPos > 700){
         rectangle.jumping = false;
         rectangle.yPos = 700;
@@ -212,9 +224,6 @@ document.getElementById("TextBox").style.opacity = "1"; // Makes it so we can se
 document.getElementById("TextBox").style.filter = 'alpha(opacity=90)';
 
 function dectectCollide(rect1, rect2) {
-    console.log(glue);
-    //console.log(rect1.yPos+rect1.height);
-    //console.log(rect2.yPos);
     if (rect1.xPos+rect1.width <= rect2.xPos + rect2.width &&
         rect1.xPos >= rect2.xPos &&
         rect1.yPos+rect1.height >= rect2.yPos-jumpVel &&
@@ -224,8 +233,8 @@ function dectectCollide(rect1, rect2) {
                 rect1.yVel = 0; 
                 if (rect1.yPos+rect1.height == rect2.yPos-jumpVel) {
                     rect1.jumping = false; }
-                if (glue == false) {
-                    glue = true;
+                if (obstacles[obstacles.indexOf(rect2)].glue == false) {
+                    obstacles[obstacles.indexOf(rect2)].glue = true;
                     currSd = rect2.speed;
                 }
                 
@@ -233,7 +242,17 @@ function dectectCollide(rect1, rect2) {
             
     
     }
-    
+    else if (
+        rect1.xPos+rect1.width <= rect2.xPos + rect2.width &&
+        rect1.xPos >= rect2.xPos &&
+        rect1.yPos>=rect2.yPos+rect2.height-(rect1.width/2) &&
+        rect1.yPos <= rect2.yPos + rect2.height
+    ) {
+        
+        rect1.yPos = rect2.yPos+rect2.height-5;
+        rect1.yVel = 0;
+        
+    }
     else if (rect1.xPos+rect1.width >= rect2.xPos &&
         rect1.xPos+rect1.width < rect2.xPos+rect2.width-(rect1.width-jumpVel) &&
         rect1.yPos < rect2.yPos + rect2.height &&
@@ -241,11 +260,7 @@ function dectectCollide(rect1, rect2) {
             rect1.xPos = rect2.xPos-rect1.width
             rect1.xVel = 0;
             
-            //let pos = obstacles.indexOf(rect2);
-            //let removedItem = obstacles.splice(pos, 1);
-            //context.clearRect(rect2.xPos, rect2.yPos, rect2.width, rect2.height)
-            //numObstacles -= 1;
-            //delete rect2;
+            
         }
         
     else if (
@@ -255,10 +270,20 @@ function dectectCollide(rect1, rect2) {
         rect1.yPos+rect1.height>rect2.yPos) {
             rect1.xPos = rect2.xPos + rect2.width;
             rect1.xVel = 0;
-            //console.log(2);
+            
     }
+    
     else {
-        glue = false;
+        obstacles[obstacles.indexOf(rect2)].glue = false;
+    }
+    if (rect2.xPos+rect2.width <= 0) {
+        let pos = obstacles.indexOf(rect2);
+        let removedItem = obstacles.splice(pos, 1);
+        //context.clearRect(rect2.xPos, rect2.yPos, rect2.width, rect2.height)
+        numObstacles -= 1;
+        score += 1;
+        delete rect2;
+        signal = true;
     }
     }
 
@@ -280,25 +305,130 @@ function drawWeather(){
     }
 }
 
-function makeObstacles() {
-    if (numObstacles <= 1) {
-        enemy = new Obstacle(550, 250, 200, "#539af6", 3);
-        let newLength = obstacles.unshift(enemy)
-    }
+function makeStairs() {
     
+    
+    //console.log(counter);
+    if (counter == -1) {
+        enemy = new Obstacle(600, 200, 150, "#539af6", globalSpeed);
+        obstacles.push(enemy)
+        counter += 1;
+        //console.log("Trueee");
+    }
+    else if (counter <3 && (obstacles[counter].xPos-50 <= CANVAS_WIDTH)) {
+        enemy = new Obstacle(600-((counter+1)*100), 200, 150, "#539af6", globalSpeed);
+        obstacles.push(enemy)
+        counter += 1;
+        }
+    
+
     obstacles.forEach(function(item, index, array) {
-        //console.log(item, index)
         item.draw();
         item.move();
         dectectCollide(rectangle, item);
     })
 }
 
+function makeBigStairs() {
+    
+    
+    //console.log(counter);
+    if (counter == -1) {
+        enemy = new Obstacle(600, 200, 150, "#539af6", globalSpeed);
+        obstacles.push(enemy)
+        counter += 1;
+        //console.log("Trueee");
+    }
+    else if (counter <3 && (obstacles[counter].xPos-50 <= CANVAS_WIDTH)) {
+        enemy = new Obstacle(600-((counter+1)*100), 200, 150, "#539af6", globalSpeed);
+        obstacles.push(enemy)
+        counter += 1;
+        }
+    else if (counter >= 3 && counter <= 4 && (obstacles[counter].xPos-100 <= CANVAS_WIDTH)) {
+        enemy = new Obstacle(600-((counter+1)*100), 200, 150, "539af6", globalSpeed);
+        obstacles.push(enemy);
+        counter += 1;
+    }
+    
+
+    obstacles.forEach(function(item, index, array) {
+        item.draw();
+        item.move();
+        dectectCollide(rectangle, item);
+    })
+}
+
+function makeArch() {
+    
+    
+    //console.log(counter);
+    if (counter == -1) {
+        enemy = new Obstacle(600, 200, 150, "#539af6", globalSpeed);
+        obstacles.push(enemy)
+        counter += 1;
+        //console.log("Trueee");
+    }
+    else if (counter <3 && (obstacles[counter].xPos-30 <= CANVAS_WIDTH)) {
+        enemy = new Obstacle(600-((counter+1)*100), 200, 150, "#539af6", globalSpeed);
+        obstacles.push(enemy)
+        counter += 1;
+        }
+    else if (counter >= 3 && counter <= 7 && (obstacles[counter].xPos-30 <= CANVAS_WIDTH)) {
+        enemy = new Obstacle(-200+((counter+1)*100), 200, 150, "539af6", globalSpeed);
+        obstacles.push(enemy);
+        counter += 1;
+    }
+    
+
+    obstacles.forEach(function(item, index, array) {
+        item.draw();
+        item.move();
+        dectectCollide(rectangle, item);
+    })
+}
+
+function makeGapStep() {
+    
+    
+    //console.log(counter);
+    if (counter == -1) {
+        enemy = new Obstacle(600, 200, 150, "#539af6", globalSpeed);
+        obstacles.push(enemy)
+        counter += 1;
+        //console.log("Trueee");
+    }
+    else if (counter < 1 && (obstacles[counter].xPos-100 <= CANVAS_WIDTH -250)) {
+        enemy = new Obstacle(600-((counter+2)*100), 200, 150, "#539af6", globalSpeed);
+        obstacles.push(enemy)
+        counter += 1;
+        }
+    
+
+    obstacles.forEach(function(item, index, array) {
+        item.draw();
+        item.move();
+        dectectCollide(rectangle, item);
+    })
+}
+
+var eventList = [makeGapStep, makeStairs, makeArch, makeBigStairs];
+
 gameLoop = function(){
+    console.log(numObstacles);
     moveRect();
     draw();
     drawWeather();
-    makeObstacles();
+    if (globalSpeed <= 50){
+        globalSpeed += 0.0005*globalSpeed;
+    }
+    if (signal == true && numObstacles == 0) {
+    
+    rand = Math.floor(Math.random()*4); 
+    signal = false;
+    counter = -1;}
+    
+    eventList[rand](); 
+    //makeObstacles();
     window.requestAnimationFrame(gameLoop);
 }
 window.addEventListener("keydown", controller.keyListener)
